8fa6f7cfa2a218212adba5c9f6ab8a84716961b7
Jared Angel <jared.aescobedo@gmail.com>
2021-01-22 14:10:50 -0600

    deploy changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
   REACT_APP_API_BASE: 'https://peaceful-retreat-79726.herokuapp.com/api' || 'http://localhost:8000/api',
-  //API_ENDPOINT: 'http://localhost:8000/api',
+//  API_ENDPOINT: 'http://localhost:8000/api',
   API_ENDPOINT: 'https://peaceful-retreat-79726.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
+  API_TOKEN: 'bed45edc-5ce7-11eb-ae93-0242ac130002',
 }
